47795dba30bb0641b5bdbf7bc8eb35d254901e81
Asim Rasheed <asimrasheed442@gmail.com>
2023-07-13 09:04:48 +0500

    login screen log remove

--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -1,12 +1,9 @@
 import { LoginInput } from "components";
 import { useAtom } from "jotai";
-import axios from "../utils/axios";
 import { useNavigate } from "react-router-dom";
 import { userAtom } from "global";
-import { useState } from "react";
 
 export default function Index() {
-  const [processing, setProcessing] = useState(false);
   const navigate = useNavigate();
   const [, setUser] = useAtom(userAtom);
   return (
@@ -21,33 +18,10 @@ export default function Index() {
           className="login__container__form"
           onSubmit={(e) => {
             e.preventDefault();
-            setProcessing(true);
-            if (e.target.email.value === "" || e.target.password.value === "") {
-              alert("Please fill all the fields");
-              setProcessing(false);
-              return;
-            } else if (e.target.email.value === "") {
-              alert("Please fill email field");
-              setProcessing(false);
-              return;
-            } else if (e.target.password.value === "") {
-              alert("Please fill password field");
-              setProcessing(false);
-              return;
-            } else {
-              axios
-                .post("/users/login", {
-                  email: e.target.email.value,
-                  password: e.target.password.value,
-                })
-                .then((res) => {
-                  setProcessing(false);
-                  setUser({
-                    name: res.data.email,
-                  });
-                  navigate("/dashboard", { replace: true });
-                });
-            }
+            setUser({
+              name: "John doe",
+            });
+            navigate("/dashboard", { replace: true });
           }}
         >
           <LoginInput
@@ -63,7 +37,7 @@ export default function Index() {
             id="password"
           />
           <button type="submit" className="login__container__form__button">
-            {processing ? "wait...." : "Login"}
+            login
           </button>
         </form>
       </div>
